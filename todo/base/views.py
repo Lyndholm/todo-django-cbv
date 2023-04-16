35b5f82aa62a7ae7c53b4a1dbd8c6ea0cfd2e22d
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-16 23:17:54 +0300

    Delete task view

--- todo/base/views.py
+++ todo/base/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
 from .models import Task
@@ -25,4 +25,10 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
