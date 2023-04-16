7fa02d0cb2f1e066a74f42c048222dc2ef74386e
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-16 23:53:53 +0300

    Login & logout

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
-from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskUpdate
+from .views import (
+    CustomLoginView,
+    CustomLogoutView,
+    TaskCreate,
+    TaskDelete,
+    TaskDetail,
+    TaskList,
+    TaskUpdate,
+)
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
@@ -8,4 +16,7 @@ urlpatterns = [
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
