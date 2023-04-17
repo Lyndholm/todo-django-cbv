9f30c7fecc412a7ca67f576dfdc4f3a3a784b5cb
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-17 22:35:58 +0300

    Search functionality

--- todo/base/views.py
+++ todo/base/views.py
@@ -48,7 +48,13 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
+
+        query = self.request.GET.get('query') or ''
+        if query:
+            context['tasks'] = context['tasks'].filter(title__contains=query)
+
         context['count'] = context['tasks'].filter(complete=False).count()
+        context['query_value'] = query
 
         return context
 
